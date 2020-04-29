89e9a0bfee3f87cfaacb5eb02533cb383683190c
Victor <vickim_@hotmail.com>
2020-04-30 00:03:06 +0200

    player collisions

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+        Module m = collision.gameObject.GetComponentInParent<Module>();
+        if (m != null)
+        {
+            if (!m.isPuny)
+            {
+                gameManager.Death();
+            }
+        }
+        else
+            print("collision fuck");
+    }
+}
