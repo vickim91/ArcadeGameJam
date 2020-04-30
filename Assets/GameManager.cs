a19996e50f7907ab56c1c63ccc97108067a3e0c7
Esben <esbennj@hotmail.com>
2020-04-30 21:33:36 +0200

    graphic stuff

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -22,7 +22,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGame();
+        }
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
     public void Death()
     {
