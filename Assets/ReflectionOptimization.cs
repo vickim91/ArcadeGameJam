4f5deb6a72721bfaffcee004b56c4ab91fd9b76f
Antonius <antonius.marcussen@gmail.com>
2020-05-03 22:40:32 +0200

    Post processing og reflection

---
+++ Assets/ReflectionOptimization.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionOptimization : MonoBehaviour
+{
+
+    ReflectionProbe refProbe;
+    public ReflectionProbe refProbe2;
+
+
+    void Start()
+    {
+        refProbe = GetComponentInParent<ReflectionProbe>();
+        refProbe.RenderProbe();
+        refProbe2.RenderProbe();
+        StartCoroutine(Timer());
+        StartCoroutine(Timer2());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(3.2f);
+        RenderReflection();
+    }
+    IEnumerator Timer2()
+    {
+        yield return new WaitForSeconds(2.3f);
+        RenderReflection2();
+    }
+
+
+    private void RenderReflection()
+    {
+        refProbe.RenderProbe();
+        StartCoroutine(Timer());
+    }
+
+    private void RenderReflection2()
+    {
+        refProbe.RenderProbe();
+        StartCoroutine(Timer2());
+    }
+}
