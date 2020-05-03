cf25c9fd1360c7d654b159eb3682d61aa58ca325
Antonius <antonius.marcussen@gmail.com>
2020-05-03 19:39:48 +0200

    destruction prefab

---
+++ Assets/DestroyDebris.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDebris : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Destroy());
+    }
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
